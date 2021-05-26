dd2699e5354f6a757a642866f811b6ba2f0aa4ab
Aman Gupta <rajeshgupta@MacBook-Air.local>
2021-05-26 18:50:02 +0530

    cloneapp

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from './images/logo.png'
-import Signup from './Signup';
+
 
 function Navbar() {
     return (
@@ -13,10 +13,10 @@ function Navbar() {
                 
 
 
-        <nav className="navbar navbar-expand-lg navbar-light  ">
-        <div className="container-fluid">
-          <NavLink exact className="navbar-brand" to='/'><img src={logo} className="logo" alt="" /></NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        <nav className="navbar navbar-expand-lg fixed-top mx-5 navbar-light  ">
+        <div className="container-fluid ">
+          <NavLink exact className="navbar-brand " to='/'><img src={logo} className="logo" alt="" /></NavLink>
+          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" 
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
                     aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
